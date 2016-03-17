395aadfb8bba8b36b65157d4ad1f89f488abf7e7
Rahmo <Rahmo.alzahrani@yahoo.com>
2016-03-16 20:27:14 -0600

    More excercises

--- CsharpLounge/CsharpLounge/CsharpStringExs.cs
+++ CsharpLounge/CsharpLounge/CsharpStringExs.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,8 +16,10 @@ namespace CsharpLounge
 {
     class CsharpStringExs
     {//1
+
         static void Main(string[] args)
         {
+
             //Setup 11
             CsharpStringExs instance = new CsharpStringExs();
 
@@ -26,27 +30,95 @@ namespace CsharpLounge
 
             //Ex.2 Reverse the words in a sentence 
             String strTest2 = "Hello, It's me ;)";
-            string result2 = instance.ReverseSentence(strTest2); 
+            string result2 = instance.ReverseSentence(strTest2);
             Console.WriteLine(result2);
 
             //Ex.3 check if string is palindrome 
             string pan1 = "cocoococ";
             string pan2 = "qqaabb";
             Console.WriteLine(pan1 + " palindrome check is " + instance.isPalindrome(pan1) + " \n" + pan2 +
-               " palindrome check is " + instance.isPalindrome(pan2));
+                              " palindrome check is " + instance.isPalindrome(pan2));
 
 
             //Ex.4 Check if String contains Only Capital Charactars
             string strcap1 = "Reeefe";
             string strcap2 = "SWEET";
-            Console.WriteLine(strcap1+" capital check is "+instance.isCapitalOnly(strcap1) + " \n" + strcap2 +
-                " capital check is "+ instance.isCapitalOnly(strcap2));
-       
+            Console.WriteLine(strcap1 + " capital check is " + instance.isCapitalOnly(strcap1) + " \n" + strcap2 +
+                              " capital check is " + instance.isCapitalOnly(strcap2));
+
+            int[] reArrange = new[] {1, 2, 3, 4, 5, 6, 7};
+            string reArrangeStr = String.Join(",", reArrange).ToString();
+            string result1 = String.Join(",", instance.arrangeMaxMInts(reArrange)).ToString();
+
+            Console.WriteLine("Rearrange an array in maximum minimum of " + reArrangeStr + " is: " + result1);
+            //Ex.5 Arrang array Input  : 
+            // arr[] = {1, 2, 3, 4, 5, 6, 7} 
+            // Output: arr[] = { 7, 1, 6, 2, 5, 3, 4 }
+
+            Console.WriteLine(Solution.result);
+
+            int[] arr = {1,2,3,3,2,1,3};
+            Console.WriteLine(" the Number Occurring Odd Number of Times " + instance.findNumberOccuringOdd(arr));
+
+
+             string Rahm = "Rahmo";
+            //string Rahm = "Rssmoo";
+            Console.WriteLine(" isUnique check  " + instance.isUniqueChar(Rahm));
+
 
             Console.Read();
         }
 
+        bool isUniqueChar(string s)
+        {
+            //this ensure all char are asci char 
+            if (s.Length > 128) return false;
+         
+            bool[] check  = new bool[128];
 
+            for (int i = 0; i < s.Length; i++)
+            {
+                //this will return the asci number of the char that is less than 128 
+                int val = s[i];
+
+                //means if the value is in the check boolean
+                if (check[val])
+                {
+                    return false;
+                }
+
+                //Save in the boo array number [Char] true
+                check[val] = true; 
+
+            }
+            return true;
+        }
+
+        //with while loop 
+        int[] arrangeMaxMInts(int[] Arr)
+        {
+            int n = Arr.Length;
+            int i = 0;
+            int k = n - 1;
+            int[] arr1 = new int[n];
+            int m = 0;
+            while (i <= k)
+            {
+                if (i == k)
+                {
+                    arr1[m] = Arr[i];
+                }
+                else {
+                    arr1[m] = Arr[k];
+                    arr1[m + 1] = Arr[i];
+                }
+                i++;
+                k--;
+                m = m + 2;
+            }
+            return arr1;
+
+        }
 
 
         /// <summary>
@@ -96,7 +168,7 @@ namespace CsharpLounge
         /// </summary>
         /// <param name="str">The string.</param>
         /// <returns><c>true</c> if the specified string is papalindrome; otherwise, <c>false</c>.</returns>
-/        bool isPalindrome(string str)
+        bool isPalindrome(string str)
         {
             if (str == null)
             {
@@ -115,6 +187,43 @@ namespace CsharpLounge
             return true; 
         }
 
+        public int findNumberOccuringOdd(int[] arr)
+        {
+
+            int count = 0;
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+
+                count = 0;
+
+                for (int j = 0; j < n; j++)
+                {
+
+                    if (arr[i] == arr[j])
+                    {
+
+                        count++;
+
+                    }
+
+                }
+
+                if (count % 2 != 0)
+
+                    return arr[i];
+
+            }
+
+            return -1;
+
+        }
+
+
 
     }
+     
+
+   
+    
 }
---
+++ CsharpLounge/CsharpLounge/TreeConstruct.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization.Configuration;
+
+namespace CsharpLounge
+{
+    class Node
+    {
+       
+            public string value;
+            public Node left;
+            public Node right;
+
+            private Node Right
+            {
+                get
+                {
+                    return right;
+                }
+
+                set
+                {
+                    right = value;
+                }
+            }
+            private Node Left
+            {
+                get
+                {
+                    return left;
+                }
+
+                set
+                {
+                    left = value;
+                }
+            }
+            public Node(string val)
+            {
+                this.value = val;
+            }
+
+
+
+        }
+        //2*5+7*20     -->                    + 
+        //                                  /     \
+        //                                 *       *
+        //                               /   \    /   \
+        //                              2   5  7  20
+       static public class Solution
+        {
+            static Node tree = new Node("+")
+            {
+                left = new Node("*")
+                {
+                    left = new Node("2"),
+                    right = new Node("5")
+                },
+                right = new Node("*") { left = new Node("7"), right = new Node("20") }
+
+            };
+
+            static public string result = ReconstructFromTreeToString(tree);
+
+           static string ReconstructFromTreeToString(Node root)
+           {
+
+            
+             string result = "";
+               if (root == null)
+               {
+                   return "";
+               }
+
+              
+               else
+               {
+
+                int temp;
+                   int temp2; 
+                   bool isChildPrint = true;
+
+          string rootval = root.value;
+                Node Current = root; 
+                   string result1 = ReconstructFromTreeToString(Current.left);
+                   string result2; 
+                  
+                     result2 = Current.value;
+                
+                string result3 = ReconstructFromTreeToString(Current.right);
+
+
+                   if (int.TryParse(result1, out temp) && int.TryParse(result3, out temp))
+                   {
+                    isChildPrint = false; 
+                   }
+                   if (result1 != "" && result3 != "" && isChildPrint == false )
+                   {
+
+
+                       result = "(" + result1 + result2 + result3 + ")";
+                   }
+                   
+                   else 
+                   {
+                    result = result1 + result2 + result3;
+                }
+               }
+               //TODO Your logic is here.
+          
+
+        return result; //The result must be returned.
+            } }
+        }
+    
+
